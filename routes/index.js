c9ac9e7f83663d1471d3de5b5df6946114d02ea3
Paulo Diovani Gonçalves <paulo@diovani.com>
2022-07-30 00:32:14 -0300

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const fs = require('fs/promises');
+const path = require('path');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (_req, res) => {
+  debugger
+  const file = path.join(__dirname, '../db/coffee-recipes.json');
+  const json = await fs.readFile(file, { encoding: 'utf-8' });
+  const recipes = JSON.parse(json);
+
+  res.render('index', { title: 'Sample express app', recipes });
 });
 
 module.exports = router;
